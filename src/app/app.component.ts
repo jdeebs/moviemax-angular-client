529cc3227135eb44770f6033ab1ebb4a14d3d1c5
Jacob DiBiase <jakesback93@hotmail.com>
2024-09-21 13:00:12 -0700

    Update root template to include buttons for signup and login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { MatDialog } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'moviemax-angular-client';
+
+  constructor(public dialog: MatDialog) {}
+  // Open the dialog when signup button is clicked
+  openUserRegistrationDialog(): void {
+    this.dialog.open(UserRegistrationFormComponent, {
+      // Dialog width
+      width: '280px',
+    });
+  }
 }
