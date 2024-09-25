b9b610755afafe80d10058dd171062fc29ff9a7d
Jacob DiBiase <jakesback93@hotmail.com>
2024-09-25 11:09:56 -0700

    Remove toolbar from rendering on welcome page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 // Angular Core & Material Design Imports
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  constructor(public router: Router) {}
   title = 'moviemax-angular-client';
+
+  // Ensure toolbar is not rendered on welcome screen
+  showToolbar(): boolean {
+    return this.router.url !== '/welcome';
+  }
 }
